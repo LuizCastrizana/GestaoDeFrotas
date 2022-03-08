74cde8ef541e356c365461dbcf9d2016f5168671
Luiz Castrizana <luiz.castrizana@gmail.com>
2022-03-07 22:21:34 -0300

    correção bug validação cpf

--- GestaoDeFrotas/GestaoDeFrotas.Front/Controllers/MotoristaController.cs
+++ GestaoDeFrotas/GestaoDeFrotas.Front/Controllers/MotoristaController.cs
@@ -415,10 +415,11 @@ namespace CadastroDeCaminhoneiro.Controllers
         }
         public ActionResult ValidaCpf(MotoristaDBE motorista)
         {
-            int idEntrada = motorista.ID;
-            motorista = new MotoristaDAL().GetByCPF(motorista.CPF, false);
-            if (motorista.ID > 0 && motorista.ID != idEntrada)
+            var motoristaBusca = new MotoristaDAL().GetByCPF(motorista.CPF, false);
+
+            if (motoristaBusca.ID > 0 && motoristaBusca.ID != motorista.ID)
                 return Json("CPF já cadastrado", JsonRequestBehavior.AllowGet);
+
             if (!CPF.ValidaCpf(motorista.CPF))
             {
                 return Json("CPF inválido", JsonRequestBehavior.AllowGet);
